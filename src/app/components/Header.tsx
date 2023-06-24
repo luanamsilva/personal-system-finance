306618bbff74d96f3d082b9f5632190884c2172b
luanamsilva <luanamoura2002@yahoo.com.br>
2023-06-23 21:52:12 -0300

    add-header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,8 @@
+
+
+export const Header = () => {
+  return (
+    <div className="flex justify-center bg-black">
+    <div className="text-white text-center text-2xl p-5">Sistema de Finanças Pessoais</div>
+ </div> )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-
+import { Header } from "@component/app/components/Header"
 export default function Home() {
   return (
     <main>
-
+<Header/>
     </main>
   )
 }
