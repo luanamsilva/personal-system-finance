fd00599a38e31a0b82a2a3598267db76e9af56ae
luanamsilva <luanamoura2002@yahoo.com.br>
2023-06-25 16:50:36 -0300

    component-transactions

--- src/app/helpers/dataFilter.ts
+++ src/app/helpers/dataFilter.ts
@@ -1,4 +1,19 @@
+import { ItemTypes } from "../types/ItemTypes"
+
 export const getCurrentMonth = () =>{
   let nowDate = new Date()
   return `${nowDate.getFullYear()} - ${nowDate.getMonth()+1}`
 }
+
+export const filteredListByMonth = (list:ItemTypes[], date: string):ItemTypes[] =>{
+  let newList: ItemTypes[] = []
+  let [year, month] = date.split('-')
+  for(let i in list ){
+    if((list[i].date.getFullYear() === parseInt(year)) &&
+     (list[i].date.getMonth()+1) === parseInt(month)){
+      newList.push(list[i])
+    }
+  }
+
+  return newList
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
+"use client"
 import { Header } from "@component/app/components/Header"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import {itemsData} from "@component/app/data/itemsData"
-import { getCurrentMonth } from "./helpers/dataFilter"
+import { getCurrentMonth, filteredListByMonth } from "./helpers/dataFilter"
+import { ItemTypes } from "./types/ItemTypes"
+import {Transactions} from "@component/app/components/Transactions"
 
 export default function Home() {
 
   const [list,setList]= useState(itemsData)
+  const[filterdList, setFilteredList] = useState<ItemTypes[]>([])
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth)
+
+  useEffect(()=>{
+  setFilteredList(filteredListByMonth(list, currentMonth)  )   
+  },[list, currentMonth])
+
   return (
     <main>
 <Header/>
+<Transactions/>
     </main>
   )
 }
