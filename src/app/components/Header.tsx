2d8bfe659bef06592794c8817a3887501dc7eeeb
luanamsilva <luanamoura2002@yahoo.com.br>
2023-06-26 22:57:32 -0300

    add-thead

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@
 
 export const Header = () => {
   return (
-    <div className="flex justify-center bg-black">
-    <div className="text-white text-center text-2xl p-5">Sistema de Finanças Pessoais</div>
+    <div className="flex justify-center bg-black w-full">
+    <div className="text-white text-center text-2xl p-5">Sistema Financeiro</div>
  </div> )
 }
--- src/app/components/Transactions.tsx
+++ src/app/components/Transactions.tsx
@@ -1,6 +1,20 @@
 export const Transactions = ()=>{
   return(
-    <>
-    </>
+    <div className="overflow-x-auto">
+    <table className="text-white w-full">
+      <thead>
+        <tr className="text-white w-full">
+          <th className="px-2 whitespace-nowrap">Data</th>
+          <th className="px-2 whitespace-nowrap">Descrição</th>
+         <th className="px-2 whitespace-nowrap">Categoria</th> 
+         <th className="px-2 whitespace-nowrap"> valor</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        
+      </tbody>
+    </table>
+    </div>
   )
 }
